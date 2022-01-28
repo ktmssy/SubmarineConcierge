649b85e3d9061e2e0456d8b368a8d93ae6ef95e1
yamaguchi rintarou <21cr0138@jec.ed.jp>
2022-01-28 12:31:23 +0900

    Save Load issue

--- Assets/Scripts/Fish/FishManager.cs
+++ Assets/Scripts/Fish/FishManager.cs
@@ -2,7 +2,7 @@
  *
  *　作成者：楊志庄
  *　作成日：2022年01月27日
- *　更新日：
+ *　更新日：2022年01月28日
  *
  ******************************
  *
@@ -25,15 +25,9 @@ namespace SubmarineConcierge.Fish
 	public class FishManager : MonoBehaviour
 	{
 		[Header("Manage")]
-		public GameObject FishPrefab;
 		public FishDatabase database;
+		private List<GameObject> wildFishes;
 
-		/* [Header("Tamed")]
-		 public bool DoMove;
-		 public float Speed;
-		 public float WaitTimeMin;
-		 public float WaitTimeMax;
-		 public Vector2 Margin;*/
 
 		private void Generate(FishIndividualData data)
 		{
@@ -44,19 +38,11 @@ namespace SubmarineConcierge.Fish
 			{
 				obj = Instantiate(fishData.PrefabTamed, Vector3.zero, Quaternion.identity);
 				obj.transform.parent = transform;
-				/*FishTamed f = obj.AddComponent<FishTamed>();
-                f.DoMove = DoMove;
-                f.Speed = Speed;
-                f.WaitTimeMin = WaitTimeMin;
-                f.WaitTimeMax = WaitTimeMax;
-                f.Margin = Margin;*/
-				//fish = f;
 			}
 			else
 			{
 				obj = Instantiate(fishData.PrefabWild, Vector3.zero, Quaternion.identity);
 				obj.transform.parent = transform;
-				//fish = obj.AddComponent<FishWild>();
 			}
 			obj.GetComponent<Fish>().Init(data, fishData);
 
@@ -64,22 +50,15 @@ namespace SubmarineConcierge.Fish
 
 		private void Start()
 		{
+			/*SaveDataManager.fishSaveData.Add(new FishIndividualData(FishType.Iwashi, 0, "hya", true));
 			SaveDataManager.fishSaveData.Add(new FishIndividualData(FishType.Iwashi, 0, "hya", true));
 			SaveDataManager.fishSaveData.Add(new FishIndividualData(FishType.Iwashi, 0, "hya", true));
-			SaveDataManager.fishSaveData.Add(new FishIndividualData(FishType.Iwashi, 0, "hya", true));
-			SaveDataManager.Save();
+			SaveDataManager.Save();*/
 
 			SaveDataManager.Load();
+			//セーブデータを使って、テイムされた魚を生成する
 			foreach (var fish in SaveDataManager.fishSaveData.Fishes)
 				Generate(fish);
-
-			/*Generate(new FishIndividualData(FishType.Iwashi, 0, "hya", true));
-
-            foreach (var fish in FindObjectsOfType<Fish>())
-            {
-                SaveDataManager.fishSaveData.Add(fish.fish);
-            }
-            SaveDataManager.Save();*/
 		}
 
 		private void Update()
--- Assets/Scripts/SaveData/SaveDataManager.cs
+++ Assets/Scripts/SaveData/SaveDataManager.cs
@@ -22,46 +22,55 @@ using UnityEngine;
 
 namespace SubmarineConcierge.SaveData
 {
-    public class SaveDataManager
-    {
-        public static FishSaveData fishSaveData = new FishSaveData();
-        public const string FishSaveDataFilename = "FishSaveData.bin";
+	public class SaveDataManager
+	{
+		public const string Postfix = ".bin";
 
-        private static T Load<T>(string filename) where T : new()
-        {
-            try
-            {
-                string path = Application.persistentDataPath + "/" + filename;
-                BinaryFormatter bf = new BinaryFormatter();
-                FileStream fs = File.Open(path, FileMode.OpenOrCreate);
-                T ret = (T)bf.Deserialize(fs);
-                fs.Close();
-                return ret;
-            }
-            catch (Exception ex)
-            {
-                return new T();
-            }
-        }
+		public static FishSaveData fishSaveData = new FishSaveData();
 
-        public static void Load()
-        {
-            fishSaveData = Load<FishSaveData>(FishSaveDataFilename);
-        }
+		public static FishTameProgressSaveData fishTameProgressSaveData = new FishTameProgressSaveData();
 
-        private static void Save(string filename, object obj)
-        {
-            string path = Application.persistentDataPath + "/" + filename;
-            Debug.Log("Save " + path);
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream fs = File.Create(path);
-            bf.Serialize(fs, obj);
-            fs.Close();
-        }
+		private static T Load<T>() where T : new()
+		{
+			try
+			{
+				string filename = fishTameProgressSaveData.GetType().Name + Postfix;
+				string path = Application.persistentDataPath + "/" + filename;
+				Debug.Log("Load " + path);
+				BinaryFormatter bf = new BinaryFormatter();
+				FileStream fs = File.Open(path, FileMode.OpenOrCreate);
+				T ret = (T)bf.Deserialize(fs);
+				fs.Close();
+				return ret;
+			}
+			catch (Exception ex)
+			{
+				Debug.Log("Load exception " + ex.Message);
+				return new T();
+			}
+		}
 
-        public static void Save()
-        {
-            Save(FishSaveDataFilename, fishSaveData);
-        }
-    }
+		public static void Load()
+		{
+			fishSaveData = Load<FishSaveData>();
+			fishTameProgressSaveData = Load<FishTameProgressSaveData>();
+		}
+
+		private static void Save(object obj)
+		{
+			string filename = obj.GetType().Name + Postfix;
+			string path = Application.persistentDataPath + "/" + filename;
+			Debug.Log("Save " + path);
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream fs = File.Create(path);
+			bf.Serialize(fs, obj);
+			fs.Close();
+		}
+
+		public static void Save()
+		{
+			Save(fishSaveData);
+			Save(fishTameProgressSaveData);
+		}
+	}
 }
