f429a10a519c0c81de7667534ffd093862e59af0
nishimura <nishimura@jec.ac.jp>
2022-01-28 12:30:46 +0900

    野生の魚の動作追加

--- Assets/Scripts/Fish/FishControl/FishWild.cs
+++ Assets/Scripts/Fish/FishControl/FishWild.cs
@@ -2,12 +2,12 @@
  *
  *　作成者：楊志庄
  *　作成日：2022年01月27日
- *　更新日：
+ *　更新日：2022年01月28日
  *
  ******************************
  *
  *　更新履歴...編集者
- *　1.
+ *　1.動作追加...神谷朋輝
  *　2.
  *　3.
  *
@@ -21,6 +21,30 @@ namespace SubmarineConcierge.Fish
 {
     public class FishWild : Fish
     {
-        
-    }
+        [SerializeField]float moveSpeed;    // 魚の移動速度
+
+		// 魚の向き
+		enum Direction
+		{
+			Left = -1,
+			Right = 1
+		}
+		[SerializeField] Direction direction;
+		private void Start()
+		{
+			// 向きに応じてスピード値の正負を変える
+			moveSpeed *= (float)direction;
+		}
+		private void Update()
+		{
+			// 魚を等速で動かす
+			this.transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+
+			// 画面外に出たら消滅
+			if (this.transform.position.x >= 20 || this.transform.position.x <= -20) 
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
 }
