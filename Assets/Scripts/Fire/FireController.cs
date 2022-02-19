b5cfc16f886581772c4e04c047391d2c1979cf08
庄庄庄庄庄 <yzz0427@vip.qq.com>
2022-02-19 15:16:52 +0900

    fire levelup effect

--- Assets/Scripts/Fire/FireController.cs
+++ Assets/Scripts/Fire/FireController.cs
@@ -13,6 +13,7 @@
  *
  ******************************/
 
+using SubmarineConcierge.Event;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,11 +25,19 @@ namespace SubmarineConcierge
         public int logExp = 1;
         private GameObject prefabFirePowder;
         TouchManager tm;
+        private GameObject prefabLevelUpEffect;
 
         private void Start()
         {
             prefabFirePowder = Resources.Load<GameObject>("Effects/FirePowder");
+            prefabLevelUpEffect = Resources.Load<GameObject>("Effects/FireLevelUp");
             tm = SingletonMB<TouchManager>.Instance;
+            UEventDispatcher.addEventListener(SCEvent.OnFireLevelUp, OnFireLevelUp);
+        }
+
+        private void OnDestroy()
+        {
+            UEventDispatcher.removeEventListener(SCEvent.OnFireLevelUp, OnFireLevelUp);
         }
 
         private void Process(Collider2D collision)
@@ -41,6 +50,11 @@ namespace SubmarineConcierge
                 SaveData.SaveDataManager.fireLevelSaveData.AddExp(logExp);
                 Destroy(collision.gameObject);
             }
+        }
+
+        private void OnFireLevelUp(UEvent e)
+        {
+            Instantiate(prefabLevelUpEffect, transform).transform.Translate(0f, 0f, -0.5f);
         }
 
 
